b619564e2e9fee47eb9790d97c075cb6458c236e
Mictronics <github@mictronics.de>
2020-11-03 22:35:43 +0100

    Layout improvements.

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,6 +1,8 @@
-renderWindSpeedChart('#windspeedChart');
+document.addEventListener('DOMContentLoaded', () => {
+  renderWindSpeedChart('#windspeedChart');
 
-renderHumidityChart('#humidityChart', 5, 30);
+  renderHumidityChart('#humidityChart', 5, 30);
 
-setInterval(renderWindSpeedChart, 1000, '#windspeedChart');
-setInterval(renderHumidityChart, 1000, '#humidityChart', 5, 30);
+  setInterval(renderWindSpeedChart, 1000, '#windspeedChart');
+  setInterval(renderHumidityChart, 1000, '#humidityChart', 5, 30);
+});
