4db62807e989e1c73f376f1629301d9dc4d63e7d
Mictronics <github@mictronics.de>
2020-11-04 21:10:20 +0100

    Add communication worker.

---
+++ client/scripts/communication.worker.js
@@ -0,0 +1,79 @@
+/*
+ * Websocket for server communication.
+ */
+let socket8080 = null;
+
+const connection = navigator.connection || navigator.mozConnection || null;
+if (connection === null) {
+  console.error('Network Information API not supported.');
+}
+
+/*
+ * Connects with port 8080
+ */
+function connect8080() {
+  console.info(`Location hostname: ${location.hostname}`);
+
+  socket8080 = new WebSocket(`ws://${location.hostname}:8080`, 'binary');
+  socket8080.binaryType = 'arraybuffer';
+
+  socket8080.onmessage = (e) => {
+    if (typeof e.data === 'string') {
+      //  string is only if CRC is NOK
+      console.error(`Wrong type of received message: ${e.data}`);
+    } else {
+      const arr = new Uint8Array(e.data);
+      let dv;
+
+      switch (arr[0] - 1) {
+        default:
+          break;
+      }
+      self.postMessage({ cmd: 'data', data: serverData });
+    }
+  };
+
+  socket8080.onclose = () => {
+    console.warn('Connection closed.');
+    socket8080 = null;
+    self.postMessage({ cmd: 'disconnected', data: null });
+    setTimeout(() => {
+      connect8080();
+    }, 1000);
+  };
+
+  /*
+   * Event functions for socket
+   */
+  socket8080.onopen = () => {
+    console.info('Connected with port 8080');
+    self.postMessage({ cmd: 'connected', data: null });
+  };
+}
+
+/*
+ * Get network connection info
+ */
+function getNetworkInfo() {
+  if (connection !== null) {
+    const { type } = connection;
+    if (type !== undefined) {
+      /* type not available in all browsers? */
+    }
+    self.postMessage({
+      cmd: 'network',
+      data: connection.downlink
+    }); /* In Mbps */
+  }
+}
+
+self.onmessage = (e) => {
+  const msg = e.data;
+  switch (msg.cmd) {
+    case 'connect':
+      connect8080();
+      break;
+    default:
+      console.error(`Unknown command: ${msg.cmd}`);
+  }
+};
