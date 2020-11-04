c74ff8ca4f48194a78ae08b2ce790a4e7dabf7e3
Mictronics <github@mictronics.de>
2020-11-04 17:42:57 +0100

    Add update of local time indication.

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,8 +1,19 @@
+// Update local time indication periodically
+function UpdateLocalTime() {
+  const d = new Date();
+  document.getElementById('localDate').innerHTML = d.toLocaleDateString();
+  document.getElementById('localTime').innerHTML = d.toLocaleTimeString();
+}
+
+// Start application as soon as DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
-  renderWindSpeedChart('#windspeedChart');
-
-  renderHumidityChart('#humidityChart', 5, 30);
-
-  setInterval(renderWindSpeedChart, 1000, '#windspeedChart');
-  setInterval(renderHumidityChart, 1000, '#humidityChart', 5, 30);
+  // Init charts
+  const windspeedChart = new WindspeedChart('#windspeedChart', 0);
+  const humidityChart = new HumidityChart('#humidityChart', 0, 0);
+  UpdateLocalTime();
+  // Init local time output
+  setInterval(UpdateLocalTime, 1000);
+  // FIXME Remove this, demo only
+  setInterval(windspeedChart.Update.bind(windspeedChart), 1000, 5);
+  setInterval(humidityChart.Update.bind(humidityChart), 1000, 20, 50);
 });
