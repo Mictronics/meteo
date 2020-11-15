be63c8b742411717ae47af7f128d766f7bfb620e
Mictronics <github@mictronics.de>
2020-11-15 19:08:54 +0100

    Add license header.

--- server/help.h
+++ server/help.h
@@ -1,3 +1,20 @@
+// Part of WebMeteo, a Vaisalla weather data visualization.
+//
+// Copyright (c) 2020 Michael Wolf <michael@mictronics.de>
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This file is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef HELP_H
 #define HELP_H
 
@@ -7,11 +24,11 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state);
 
 enum
 {
-    OPTSERIAL = 1000,
-    OPTBAUDRATE,
-    OPTGROUP,
-    OPTUSER,
-    OPTNOGPS
+        OPTSERIAL = 1000,
+        OPTBAUDRATE,
+        OPTGROUP,
+        OPTUSER,
+        OPTNOGPS
 };
 
 static struct argp_option options[] =
--- server/timer.c
+++ server/timer.c
@@ -1,4 +1,23 @@
-//https://qnaplus.com/implement-periodic-timer-linux/
+// Part of WebMeteo, a Vaisalla weather data visualization.
+//
+// Based on work by Srikanta Sing
+// See https://qnaplus.com/implement-periodic-timer-linux/
+//
+// Copyright (c) 2020 Michael Wolf <michael@mictronics.de>
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This file is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <unistd.h>
 #include <stdint.h>
 #include <string.h>
